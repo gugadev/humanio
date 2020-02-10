a9b8413469a6d67760831e558b0c0e7c92b068a6
gugadev <gusgarzaki@gmail.com>
2020-02-10 15:18:30 -0500

    update app module import new modules

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,15 @@
 import { join } from 'path'
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { SkillModule } from './skill/skill.module';
+import { ClientModule } from './client/client.module';
+import { EmployeeModule } from './employee/employee.module';
+import { Skill } from './skill/skill.model';
+import { Connection } from 'typeorm'
+import { Client } from './client/client.model';
+
+console.log(join(process.cwd(), 'data.db'))
 
 @Module({
   imports: [
@@ -12,8 +20,21 @@ import { SkillModule } from './skill/skill.module';
         outputAs: 'class'
       }
     }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: join(process.cwd(), 'data.db'),
+      entities: [Skill, Client],
+      logging: true,
+      synchronize: false
+    }),
     // app modules
-    SkillModule
+    ClientModule,
+    SkillModule,
+    EmployeeModule
   ]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {
+    console.log(this.connection.isConnected)
+  }
+}
