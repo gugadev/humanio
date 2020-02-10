7ce870cb95cd2e9da4049018b34256902a9b2e5f
gugadev <gusgarzaki@gmail.com>
2020-02-10 15:17:38 -0500

    create client module

---
+++ api/src/client/client.module.ts
@@ -0,0 +1,21 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ClientResolver } from "./client.resolver";
+import { ClientService } from "./client.service";
+import { Client } from "./client.model";
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Client])
+  ],
+  providers: [
+    ClientResolver,
+    ClientService
+  ],
+  exports: [
+    ClientService
+  ]
+})
+export class ClientModule {
+  
+}
