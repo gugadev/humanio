e47dc90ad762dd3418388caae2a50d341aa9b715
gugadev <gusgarzaki@gmail.com>
2020-02-10 15:17:09 -0500

    create skill module

--- api/src/skill/skill.module.ts
+++ api/src/skill/skill.module.ts
@@ -1,8 +1,21 @@
-import { Module } from "@nestjs/common";
-import { SkillResolver } from "./skill.resolver";
+import { Module } from "@nestjs/common"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { SkillResolver } from "./skill.resolver"
+import { SkillService } from "./skill.service"
+import { Skill } from "./skill.model"
 
 @Module({
-  providers: [SkillResolver]
+  imports: [
+    TypeOrmModule.forFeature([Skill]),
+  ],
+  providers: [
+    SkillService,
+    SkillResolver
+  ],
+  exports: [
+    // TypeOrmModule,
+    SkillService
+  ]
 })
 export class SkillModule {
   
