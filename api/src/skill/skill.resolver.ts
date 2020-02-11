877a13571900f59d0dd131860bb56749346161c2
gugadev <gusgarzaki@gmail.com>
2020-02-11 11:29:58 -0500

    use typings

--- api/src/skill/skill.resolver.ts
+++ api/src/skill/skill.resolver.ts
@@ -1,12 +1,13 @@
 import { Resolver, Query } from "@nestjs/graphql"
 import { SkillService } from "./skill.service"
+import { Skill } from "./skill.model"
 
 @Resolver('Skill')
 export class SkillResolver {
   constructor(private readonly service: SkillService) {}
 
   @Query('skills')
-  async getAll() {
+  async getAll(): Promise<Skill[]> {
     return this.service.getAll()
   }
 }
