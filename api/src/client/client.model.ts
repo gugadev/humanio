6e3b9632713e70bc49e33bfcae86dd353969bc65
gugadev <gusgarzaki@gmail.com>
2020-02-11 11:28:42 -0500

    add relationship with Employee

--- api/src/client/client.model.ts
+++ api/src/client/client.model.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm'
+import { Employee } from 'src/employee/employee.model'
 
 @Entity('clients')
 export class Client {
@@ -6,4 +7,6 @@ export class Client {
   id: number
   @Column()
   name: string
+  @OneToMany(() => Employee, emp => emp.client)
+  employees: Employee[]
 }
