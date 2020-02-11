18a65e507577cc213e479df8b534bdd6cb4e16ae
gugadev <gusgarzaki@gmail.com>
2020-02-11 11:29:40 -0500

    set typings

--- api/src/client/client.resolver.ts
+++ api/src/client/client.resolver.ts
@@ -1,12 +1,13 @@
 import { Resolver, Query } from "@nestjs/graphql";
 import { ClientService } from "./client.service";
+import { Client } from "./client.model";
 
 @Resolver('Client')
 export class ClientResolver {
   constructor(private readonly service: ClientService) {}
 
   @Query('clients')
-  async getAll() {
+  async getAll(): Promise<Client[]> {
     return this.service.getAll()
   }
 }
